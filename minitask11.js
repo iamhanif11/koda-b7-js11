8a8ee43d8e4fe1b9c22d7f0ba5ba1bd5f31cbab0
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-27 15:35:58 +0700

    Add files via upload

---
+++ minitask11.js
@@ -0,0 +1,9 @@
+function tampilkan(name, delay) {
+    return new Promise((resolve, reject) => {
+        const name = true;
+        setTimeout(() => {
+            resolve ("John", "Ed", "Jane")
+        }, 1500, 2000, 500);       
+    })
+}
+
