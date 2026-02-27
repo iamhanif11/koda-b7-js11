274bbba24a79968cc399c8aefad4fad33a0b1f13
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-27 16:39:36 +0700

    Delete minitask11.js

--- minitask11.js
+++
@@ -1,9 +0,0 @@
-function tampilkan(name, delay) {
-    return new Promise((resolve, reject) => {
-        const name = true;
-        setTimeout(() => {
-            resolve ("John", "Ed", "Jane")
-        }, 1500, 2000, 500);       
-    })
-}
-
